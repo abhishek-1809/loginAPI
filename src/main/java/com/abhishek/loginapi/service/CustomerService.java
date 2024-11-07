c0962ec79a5a8d70a2c1ea45bd8be9850f1f193f
abhishek-1809 <abhisahu1920@gmail.com>
2024-11-07 21:15:40 +0530

    Update CustomerService.java

--- src/main/java/com/abhishek/loginapi/service/CustomerService.java
+++ src/main/java/com/abhishek/loginapi/service/CustomerService.java
@@ -10,15 +10,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
-    public String createCustomer(CustomerRequest request) {
+    public CustomerResponse createCustomer(CustomerRequest request) {
         Customer customer = mapper.toEntity(request);
-        repo.save(customer);
-        return "Created";
+        Customer savedCustomer = repo.save(customer);
+        return mapper.toDto(savedCustomer);
     }
+
 }
