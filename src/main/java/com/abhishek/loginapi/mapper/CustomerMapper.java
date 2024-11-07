9aca606b09154455089c3bcb777525b1d4437243
abhishek-1809 <abhisahu1920@gmail.com>
2024-11-07 21:15:31 +0530

    Update CustomerMapper.java

--- src/main/java/com/abhishek/loginapi/mapper/CustomerMapper.java
+++ src/main/java/com/abhishek/loginapi/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.abhishek.loginapi.mapper;
 
 import com.abhishek.loginapi.dto.CustomerRequest;
+import com.abhishek.loginapi.dto.CustomerResponse;
 import com.abhishek.loginapi.entity.Customer;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,13 @@ public class CustomerMapper {
                 .email(request.email())
                 .password(request.password())
                 .build();
+    }
+
+    public CustomerResponse toDto(Customer customer) {
+        return new CustomerResponse(
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail()
+                );
     }
 }
